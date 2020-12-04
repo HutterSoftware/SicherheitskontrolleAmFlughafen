840a5547875f1f4593616ca1a3a4e619777ed3e0
PhiJoKo <philipp@kochonline.de>
2020-12-04 18:15:35 +0100

    TestSecurity class

---
+++ 02_implementation/src/test/java/TestSecurity.java
@@ -0,0 +1,74 @@
+import org.junit.Test;
+
+public class TestSecurity {
+
+    @Test
+    public void simulationTest() {
+
+    }
+
+    @Test
+    public void employeePositionTest() {
+
+    }
+
+    @Test
+    public void cancelIDCardTest() {
+
+    }
+
+    @Test
+    public void scannerLoginTest() {
+
+    }
+
+    @Test
+    public void employeeProfileTest() {
+
+    }
+
+    @Test
+    public void unlockScannerTest() {
+
+    }
+
+    @Test
+    public void knifeTest() {
+
+    }
+
+    @Test
+    public void weaponTest() {
+
+    }
+
+    @Test
+    public void explosivesTest() {
+
+    }
+
+    @Test
+    public void scanRecordTest() {
+
+    }
+
+    @Test
+    public void noProhibitedProcessTest() {
+
+    }
+
+    @Test
+    public void knifeProcessTest() {
+
+    }
+
+    @Test
+    public void weaponProcessTest() {
+
+    }
+
+    @Test
+    public void explosivesProcessTest() {
+
+    }
+}
