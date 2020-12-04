7c7fc195c876713c0b2ce8f402090eaba70ab4f7
PhiJoKo <philipp@kochonline.de>
2020-12-04 20:00:03 +0100

    scan for knife test

--- 02_implementation/src/test/java/TestSecurity.java
+++ 02_implementation/src/test/java/TestSecurity.java
@@ -1,4 +1,17 @@
+import components.Scanner;
+import components.Tray;
+import data.Record;
+import data.ScanResult;
 import org.junit.Test;
+import passenger.HandBaggage;
+import passenger.Layer;
+import passenger.Passenger;
+import simulation.Configuration;
+
+import java.io.*;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestSecurity {
 
@@ -33,8 +46,15 @@ public class TestSecurity {
     }
 
     @Test
-    public void knifeTest() {
+    public void knifeTest() throws IOException, URISyntaxException {
 
+        HandBaggage baggage = TestUtils.createBaggage("knife_baggage.txt");
+
+        data.Record record = TestUtils.scanBaggage(baggage);
+        ScanResult result = record.getResult();
+
+        assertEquals("PROHIBITED", result.getItemType());
+        assertEquals("kn!fe", result.getProhibitedItemType());
     }
 
     @Test
